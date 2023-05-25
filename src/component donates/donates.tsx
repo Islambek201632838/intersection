2a3b5e3433cfd78f61fb0c156aa3ba09c912510d
Isevich_savage <islambek.sadykov@nu.edu.kz>
2023-05-26 05:48:03 +0600

    donates add2

--- src/component donates/donates.tsx
+++ src/component donates/donates.tsx
@@ -386,6 +386,10 @@ return (
                 <option value="">2023</option>
                 <option value="">2024</option>
                 <option value="">2025</option>
+                <option value="">2026</option>
+                <option value="">2027</option>
+                <option value="">2028</option>
+                <option value="">2029</option>
             </select>  
 
         <p className="rightText3">Делайте это ежемесячно !</p>
